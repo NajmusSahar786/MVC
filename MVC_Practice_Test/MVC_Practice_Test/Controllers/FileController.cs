01319b79fbcda0b1d905414e54e4e35312bcd4b3
Najmus Sahar <najmus.sahar77@gmail.com>
2021-01-23 18:06:48 +0530

    multiple file upload

---
+++ MVC_Practice_Test/MVC_Practice_Test/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Practice_Test.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UploadFiles()
+        {
+            string uname = Request["uploadername"];
+            HttpFileCollectionBase files = Request.Files;
+            for (int i = 0; i < files.Count; i++)
+            {
+                HttpPostedFileBase file = files[i];
+                string fname;
+                // Checking for Internet Explorer      
+                if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                {
+                    string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                    fname = testfiles[testfiles.Length - 1];
+                }
+                else
+                {
+                    fname = file.FileName;
+                }
+                // Get the complete folder path and store the file inside it.      
+                fname = Path.Combine(Server.MapPath("~/Uploads/"), fname);
+                file.SaveAs(fname);
+            }
+            return Json("Hi, " + uname + ". Your files uploaded successfully", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
